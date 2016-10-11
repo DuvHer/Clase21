ca57dc344c80716e99438dbab266d5998547e69b
Estudiante <Estudiante@FI401104.unal.loc>
2016-10-11 15:39:30 -0500

    Clase 21

---
+++ src/clase9/A.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase9;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class A {
+        public int x;
+        
+        public A(int x){
+        this.x=x;
+        }
+        public void yy()
+        {
+         System.out.println("yy");
+        }
+}
---
+++ src/clase9/B.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase9;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class B extends A{
+    public int y;
+    
+    public B(int x, int y){
+    super(x);
+    this.y=y;
+    }
+    //@Override
+    public void yy()
+    {
+        System.out.println("xx"+ x);
+    }
+}
